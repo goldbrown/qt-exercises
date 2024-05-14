184ca338f40558483bfd8173af1c7c1502f141a3
goldbrown <chrisliu1314@qq.com>
2024-05-14 22:41:13 +0800

    qt container

--- QtTools/main.cpp
+++ QtTools/main.cpp
@@ -5,6 +5,8 @@
 #include <QPushButton>
 #include <QDebug>
 
+void qtIterator();
+
 int main(int argc, char *argv[])
 {
 
@@ -30,7 +32,40 @@ int main(int argc, char *argv[])
     qInfo() << ba;
     qInfo() << ba.append(" some char");
 
+    // qt list
+    QList<QString> list;
+    QString str("this is a test string");
+    list << str;
+    qInfo() << list[0] + " how are you";
+
+    QList<int> intlist;
+    intlist << 1 << 2 << 3 << 4 << 5;
+    QListIterator<int> iter(intlist);
+    for(;iter.hasNext();) {
+        qInfo() << iter.next();
+    }
+
+    // qt iterator
+    qtIterator();
 
 
     return 0;
+}
+
+void qtIterator()
+{
+    QList<int> list;
+    for (int i = 0; i < 10; i++) {
+        list.insert(list.end(), i);
+    }
+
+    QList<int>::iterator iter;
+    for(iter = list.begin(); iter != list.end(); iter++) {
+        qInfo() << *iter;
+        *iter = (*iter) * 10;
+    }
+    QList<int>::const_iterator ci;
+    for(ci = list.constBegin(); ci != list.constEnd(); ci++) {
+        qInfo() << *ci;
+    }
 }
