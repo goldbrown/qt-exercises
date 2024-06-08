cec887e553a4d331db0d98fe15f33be44f2ce813
goldbrown <1978151299@qq.com>
2024-06-08 23:06:12 +0800

    mouse event && network request

---
+++ MyMouseEvent/mouseevent.cpp
@@ -0,0 +1,56 @@
+#include "mouseevent.h"
+
+MouseEvent::MouseEvent(QWidget *parent)
+    : QMainWindow(parent)
+{
+    setWindowTitle(tr("mouse event"));
+    statusLabel = new QLabel();
+    statusLabel->setText(tr("current position:"));
+    statusLabel->setFixedWidth(100);
+
+    mousePosLabel = new QLabel();
+    mousePosLabel->setText(tr(""));
+    mousePosLabel->setFixedWidth(100);
+    statusBar()->addPermanentWidget(statusLabel);
+    statusBar()->addPermanentWidget(mousePosLabel);
+    this->setMouseTracking(true);
+    resize(400, 200);
+}
+
+MouseEvent::~MouseEvent()
+{
+}
+
+void MouseEvent::mousePressEvent(QMouseEvent *e)
+{
+    QString str = "(" + QString::number(e->x()) + "," + QString::number(e->y()) + ")";
+    if (e->button() == Qt::LeftButton)
+    {
+        statusBar()->showMessage(tr("left button:") + str);
+    }
+    else if (e->button() == Qt::RightButton)
+    {
+        statusBar()->showMessage(tr("right button:") + str);
+    }
+    else if (e->button() == Qt::MidButton)
+    {
+        statusBar()->showMessage(tr("middle button:") + str);
+    }
+}
+
+void MouseEvent::mouseMoveEvent(QMouseEvent *e)
+{
+    mousePosLabel->setText("(" + QString::number(e->x()) + "," + QString::number(e->y()) + ")");
+}
+
+void MouseEvent::mouseReleaseEvent(QMouseEvent *e)
+{
+    QString str = "(" + QString::number(e->x()) + "," + QString::number(e->y()) + ")";
+    statusBar()->showMessage("release at:" + str, 3000);
+}
+
+void MouseEvent::mouseDoubleClickEvent(QMouseEvent *e)
+{
+
+}
+
